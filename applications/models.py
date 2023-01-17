f2c6030c2ebbb56490fb5e90d94481e1d80a84ca
crowdbotics-apps <team@crowdbotics.com>
2023-01-17 22:47:21 +0000

    Update models

---
+++ applications/models.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.db import models
+class Application(models.Model):
+    'Generated Model'
+    namee = models.CharField(max_length=256,)
+
+# Create your models here.
--- crwdbtcsboard_38334/settings.py
+++ crwdbtcsboard_38334/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites', "plans"
+    'django.contrib.sites', "plans", "applications"
 ]
 LOCAL_APPS = [
     'home',
