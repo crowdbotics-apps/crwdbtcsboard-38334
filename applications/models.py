938069df50f7708ba28f8655b75ee4f715ad06d9
vinicodex <mvnxr28@gmail.com>
2023-01-18 20:22:05 -0300

    remove duplicated app

--- applications/models.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-from django.db import models
-class Application(models.Model):
-    'Generated Model'
-    name = models.CharField(max_length=256,)
-
-# Create your models here.
