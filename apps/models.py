6bc63a63aa2f39a3da593d5831359ac316ee205a
vinicodex <mvnxr28@gmail.com>
2023-01-18 16:00:35 -0300

    updated choices

--- apps/models.py
+++ apps/models.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.db import models
-from django.core.validators import MinLengthValidator
 from enum import Enum
 
 
@@ -10,26 +8,25 @@ class ApplicationType(Enum):
 
     @classmethod
     def choices(cls):
-        return tuple((i.name, i.value) for i in cls)
-
-
-class ApplicationFramework(Enum):
-    DJANGO = "Django"
-    REACT = "React Native"
-
-    @classmethod
-    def choices(cls):
-        return tuple((i.name, str(i.value)) for i in cls)
+        return list(tuple((i.name, i.value) for i in cls))
 
 
 class Application(models.Model):
     'Generated Model'
 
+    DJANGO = 'Django'
+    REACT = 'React'
+    APP_FRAMEWORKS = [
+        (DJANGO, 'Django'),
+        (REACT, 'React'),
+    ]
+
     name = models.CharField("Name", max_length=50, blank=False)
     description = models.TextField("Description",
                                    blank=False, max_length=20)
     type = models.CharField("Description", max_length=50, choices=ApplicationType.choices(), blank=False, default=None)
-    framework = models.CharField("Framework", max_length=50, choices=ApplicationFramework.choices(), blank=False, default=None)
+    framework = models.CharField("Framework", max_length=50, choices=APP_FRAMEWORKS, blank=False,
+                                 default=None)
     domain_name = models.CharField("Domain Name", max_length=50, blank=False)
 
     def __str__(self):
