e47f75f5fa3bb3c84ce8d01d255d12d80ff87dc4
crowdbotics-apps <team@crowdbotics.com>
2023-01-18 17:08:54 +0000

    Update models

---
+++ apps/models.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.db import models
+class Application(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=50,)
+
+# Create your models here.
