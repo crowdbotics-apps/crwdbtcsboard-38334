a417eecd569dc48707306abed27aca931594c1e4
vinicodex <mvnxr28@gmail.com>
2023-01-18 14:59:04 -0300

    setup application app and viewset permission

--- apps/api/v1/viewsets.py
+++ apps/api/v1/viewsets.py
@@ -1,9 +1,12 @@
 from rest_framework import authentication
+from rest_framework.permissions import IsAuthenticated
 from apps.models import Application
 from .serializers import ApplicationSerializer
 from rest_framework import viewsets
 
+
 class ApplicationViewSet(viewsets.ModelViewSet):
     serializer_class = ApplicationSerializer
     authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
     queryset = Application.objects.all()
--- apps/models.py
+++ apps/models.py
@@ -1,7 +1,40 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import MinLengthValidator
+from enum import Enum
+
+
+class ApplicationType(Enum):
+    WEB = "Web"
+    MOBILE = "Mobile"
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
+
+class ApplicationFramework(Enum):
+    DJANGO = "Django"
+    REACT = "React Native"
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
+
 class Application(models.Model):
     'Generated Model'
-    name = models.CharField(max_length=50,)
 
-# Create your models here.
+    name = models.CharField("Name", max_length=50, blank=False)
+    description = models.TextField("Description",
+                                   blank=False,
+                                   validators=[MinLengthValidator(1,
+                                                                  'the field must contain at least 1 characters'),
+                                               ]
+                                   )
+    type = models.CharField("Description", max_length=50, choices=ApplicationType.choices(), blank=False, default=None)
+    framework = models.CharField("Framework", max_length=50, choices=ApplicationFramework.choices(), blank=False, default=None)
+    domain_name = models.CharField("Domain Name", max_length=50, blank=False)
+
+    def __str__(self):
+        return f"{self.name} - {self.framework}"
