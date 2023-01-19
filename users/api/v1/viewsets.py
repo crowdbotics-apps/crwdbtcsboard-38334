26b433c10a7a8a55844a2ea58cdea3a67bea0865
vinicodex <mvnxr28@gmail.com>
2023-01-19 13:32:34 -0300

    update user viewsets.py

--- users/api/v1/viewsets.py
+++ users/api/v1/viewsets.py
@@ -1,10 +1,13 @@
+import token
+
 from rest_framework import authentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 from rest_framework.response import Response
 from users.models import User
 from .serializers import UserSerializer
 from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,6 +17,28 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
 
     def list(self, request, *args, **kwargs):
-        user = User.objects.all().first()
-        serializer = UserSerializer(user)
+        request_token = Token.objects.filter(key=request.auth).first()
+        if not request_token:
+            return Response(status=HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(request_token.user)
         return Response(status=HTTP_200_OK, data=serializer.data)
+
+    def retrieve(self, request, *args, pk=None):
+        request_token = Token.objects.filter(key=request.auth).first()
+        if not request_token:
+            return Response(status=HTTP_404_NOT_FOUND)
+        queryset = request_token.user.subscription_user.filter(pk=pk).first()
+        data = {
+            "name": request_token.user.name,
+            "email": request_token.user.email,
+            "subscription": [
+                {
+                    "id":queryset.app.pk,
+                    "name":queryset.app.description,
+                    "description":queryset.app.description,
+                    "domain":queryset.app.domain_name,
+                    "framework":queryset.app.framework,
+                }
+            ]
+        }
+        return Response(status=HTTP_200_OK, data=data)
